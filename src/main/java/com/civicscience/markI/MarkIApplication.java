994133d7f3e7489c58578138ca12b30303f68cb0
Santiago Garcia <santiagogarcia@ip-192-168-1-37.ec2.internal>
2022-12-14 08:22:10 -0500

    feat: init project

---
+++ src/main/java/com/civicscience/markI/MarkIApplication.java
@@ -0,0 +1,13 @@
+package com.civicscience.markI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkIApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/civicscience/markI/MarkIApplicationTests.java
@@ -0,0 +1,13 @@
+package com.civicscience.markI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkIApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
